6e5d64d9b1a7867083a6b83bdc041eea6cba8e96
Angeloni Cuesta <angelonicuesta@Angelonis-MacBook-Pro.local>
2022-10-28 15:21:08 -0300

    styling table

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <>
-    <div className="bg-white">
+    <div className="h-screen bg-white">
     <div className='pt-6'>
       <div className='md:grid md:grid-cols-4 md:gap-1'>
         <Breadcrumb></Breadcrumb>
@@ -15,7 +15,40 @@ export default function Home() {
           <Select />
         </div>
       </div>
-      
+      <div className='p-10'>
+        <div className='bg-gray overflow-auto rounded-lg shadow'>
+        <table className='w-full'>
+        <thead className='bg-gray-50 border-b-2 border-gray-200'>
+          <tr>
+            <th className='p-3 text-sm font-semibold tracking-wide text-left'>Comment </th>
+            <th className='w-60 p-3 text-sm font-semibold tracking-wide text-left'>Authored</th>
+            <th className='w-60 p-3 text-sm font-semibold tracking-wide text-left'>Commited By</th>
+            <th className='w-32 p-3 text-sm font-semibold tracking-wide text-left'>Date</th>
+            <th className='w-32 p-3 text-sm font-semibold tracking-wide text-left'>URL</th>
+            <th className='w-32 p-3 text-sm font-semibold tracking-wide text-left'>Hash</th>
+          </tr>
+        </thead>
+          <tbody className='divide-y divide-gray-100'>
+            <tr className='bg-white'>
+              <td className='p-3 text-sm text-gray-700 whitespace-nowrap'>init commit</td>
+              <td className='w-60 p-3 text-sm text-gray-700 whitespace-nowrap'> Angeloni Cuesta</td>
+              <td className='w-60 p-3 text-sm text-gray-700 whitespace-nowrap'> Angeloni Cuesta</td>
+              <td className='w-32 p-3 text-sm text-gray-700 whitespace-nowrap'> today</td>
+              <td className='w-32 p-3 text-sm text-gray-700 whitespace-nowrap'><a href='#' className='text-blue-500 hover:underline' >url</a> </td>
+              <td className='w-32 p-3 text-sm text-gray-700 whitespace-nowrap'> 1234wsafg32</td>
+            </tr>
+            <tr className='bg-gray-50'>
+              <td className='p-3 text-sm text-gray-700 whitespace-nowrap'>init commit</td>
+              <td className='w-60 p-3 text-sm text-gray-700 whitespace-nowrap'> Angeloni Cuesta</td>
+              <td className='w-60 p-3 text-sm text-gray-700 whitespace-nowrap'> Angeloni Cuesta</td>
+              <td className='w-32 p-3 text-sm text-gray-700 whitespace-nowrap'> today</td>
+              <td className='w-32 p-3 text-sm text-gray-700 whitespace-nowrap'><a href='#' className='text-blue-500 hover:underline' >url</a> </td>
+              <td className='w-32 p-3 text-sm text-gray-700 whitespace-nowrap'> 1234wsafg32</td>
+            </tr>
+          </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   </div>
       
