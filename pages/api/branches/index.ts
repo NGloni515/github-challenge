0e754b47f6b25119959f7d28f379488050b2f4c8
Angeloni Cuesta <angelonicuesta@Angelonis-MacBook-Pro.local>
2022-10-28 16:41:14 -0300

    get branches endpoint

---
+++ pages/api/branches/index.ts
@@ -0,0 +1,35 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import axios from 'axios'
+
+type Data = 
+| { msg: string }
+| { branches: [] | null}
+
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  
+  
+  switch( req.method ) {
+    case 'GET':
+      return getBranches(req, res)
+    default:
+      return res.status(400).json({
+        msg: 'Bad request'
+      })
+  }
+  
+}
+
+export const getBranches = async(req: NextApiRequest, res: NextApiResponse<Data>) => {
+    console.log("GET BRANCHES: ")
+  const branches : any | null = (await axios.get("https://api.github.com/repos/NGloni515/github-challenge/branches",{'headers':{'Authorization': "token "+process.env.GITHUB_PRIVATE_KEY}})).data
+  console.log("branches: ",branches)
+  if(branches){
+    return res.status(201).json({ branches, msg:'Branches data retrieved succesfully' })
+  }else{
+    return res.status(404).json({ msg:'Branches do not exist' })
+  }
+}
