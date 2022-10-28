7fd3043159295c3500207911a71f8f56d6d55fbf
Angeloni Cuesta <angelonicuesta@Angelonis-MacBook-Pro.local>
2022-10-28 19:09:50 -0300

    fetch data from home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,37 @@
+import { useState, useEffect } from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
 import Breadcrumb from '../components/Breadcrumb'
 import Select from '../components/Select'
 import Table from '../components/Table'
-import styles from '../styles/Home.module.css'
+import { getHomeData } from '../requests';
 
 export default function Home() {
+  const [currentBranch, setCurrentBranch] = useState({name: 'main'})
+  const [repository, setRepository] = useState({})
+  const [branches, setBranches] = useState([])
+  const [commits, setCommits] = useState([])
+  
+
+  useEffect(() => {
+    const setFetchData = async() => {
+        const {repository, branches, commits} :any = await getHomeData(currentBranch.name);
+        if(repository){
+          setRepository(repository)
+          setBranches(branches)
+          setCommits(commits)
+        }
+        console.log("branches: ",branches)
+    }
+    setFetchData();
+}, []);
+
   return (
     <>
     <div className="h-screen bg-white">
     <div className='pt-6'>
       <div className='md:grid md:grid-cols-4 md:gap-1'>
-        <Breadcrumb />
+        <Breadcrumb repository={repository} />
         <div className='w-56'>
           <Select />
         </div>
